ad0757304c08fc068d7db43ea8949080df95bc77
Braun Goodson <braungoodson@gmail.com>
2014-12-25 20:35:38 -0500

    ok

--- eg-socket.js
+++ eg-socket.js
@@ -1,32 +1,15 @@
 $SocketFactory.$inject = ['$rootScope'];
 function $SocketFactory($rootScope) {
   var socket = io.connect();
-  var SocketFactory = {
-    on: function on(eventName,callback) {
-      socket.on(eventName,function(){
-        var args = arguments;
-        // This tells AngularJS that it needs to 
-        // check the state of the application and 
-        // update the templates if there was a 
-        // change after running the callback passed to it.
-        $rootScope.$apply(function(){
-          callback.apply(socket,args);
-        });
-      });
-    },
-    emit: function emit(eventName,data,callback) {
-      socket.emit(eventName,data,function(){
-        var args = arguments;
-        $rootScope.$apply(function(){
-          if (callback) {
-            callback.apply(socket,args);
-          }
-        });
-      });
-    }
+  socket.on('broadcast:all',function(data){
+    $rootScope.$broadcast('broadcast:all',{data:data});
+  });
+  var SocketFactory = function() {};
+  SocketFactory.prototype.broadcast = function(data) {
+    socket.emit('broadcast:all',{data:data});
   };
   return SocketFactory;
 }
 
-angular.module('egSocket',[])
-  .factory('socket',$SocketFactory);
+angular.module('eg.socket',[])
+  .factory('SocketFactory',$SocketFactory);
